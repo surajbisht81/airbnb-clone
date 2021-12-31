d4149bdbbe89ab49b7cbdbd5e20c416ea8b70469
Suraj Bisht <surajbishtsb81@gmail.com>
2021-12-31 20:57:04 +0530

    everything is good

--- pages/search.js
+++ pages/search.js
@@ -1,9 +1,9 @@
-import Footer from "./Components/Footer"
-import Header from "./Components/Header"
+import Footer from "./Footer"
+import Header from "./Header"
 import { useRouter } from 'next/router';
 import { format } from "date-fns";
-import InfoCard from "./Components/InfoCard";
-import Map from "./Components/Map";
+import InfoCard from "./InfoCard";
+import Map from "./Map";
 
 export async function getServerSideProps() {
     const searchResults = await fetch("https://links.papareact.com/isz")
